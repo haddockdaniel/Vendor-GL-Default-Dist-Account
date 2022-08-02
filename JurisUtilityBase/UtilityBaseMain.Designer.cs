3055225602445379fc199997b4267a258d48fcc1
haddockdaniel <43544084+haddockdaniel@users.noreply.github.com>
2022-08-02 09:27:36 -0400

    Initial commit

---
+++ JurisUtilityBase/UtilityBaseMain.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace JurisUtilityBase
+{
+    partial class UtilityBaseMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UtilityBaseMain));
+            this.JurisLogoImageBox = new System.Windows.Forms.PictureBox();
+            this.LexisNexisLogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBoxCompanies = new System.Windows.Forms.ListBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.OpenFileDialogOpen = new System.Windows.Forms.OpenFileDialog();
+            this.buttonReport = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cbTo = new System.Windows.Forms.ComboBox();
+            this.cbFrom = new System.Windows.Forms.ComboBox();
+            this.statusGroupBox = new System.Windows.Forms.GroupBox();
+            this.labelCurrentStatus = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelPercentComplete = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.JurisLogoImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexisNexisLogoPictureBox)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            this.statusGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // JurisLogoImageBox
+            // 
+            this.JurisLogoImageBox.Image = ((System.Drawing.Image)(resources.GetObject("JurisLogoImageBox.Image")));
+            this.JurisLogoImageBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("JurisLogoImageBox.InitialImage")));
+            this.JurisLogoImageBox.Location = new System.Drawing.Point(0, 1);
+            this.JurisLogoImageBox.Name = "JurisLogoImageBox";
+            this.JurisLogoImageBox.Size = new System.Drawing.Size(104, 336);
+            this.JurisLogoImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.JurisLogoImageBox.TabIndex = 0;
+            this.JurisLogoImageBox.TabStop = false;
+            // 
+            // LexisNexisLogoPictureBox
+            // 
+            this.LexisNexisLogoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("LexisNexisLogoPictureBox.Image")));
+            this.LexisNexisLogoPictureBox.Location = new System.Drawing.Point(8, 343);
+            this.LexisNexisLogoPictureBox.Name = "LexisNexisLogoPictureBox";
+            this.LexisNexisLogoPictureBox.Size = new System.Drawing.Size(96, 28);
+            this.LexisNexisLogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.LexisNexisLogoPictureBox.TabIndex = 1;
+            this.LexisNexisLogoPictureBox.TabStop = false;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 391);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(658, 22);
+            this.statusStrip.TabIndex = 2;
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(135, 17);
+            this.toolStripStatusLabel.Text = "Status: Ready to Execute";
+            // 
+            // listBoxCompanies
+            // 
+            this.listBoxCompanies.FormattingEnabled = true;
+            this.listBoxCompanies.Location = new System.Drawing.Point(111, 1);
+            this.listBoxCompanies.Name = "listBoxCompanies";
+            this.listBoxCompanies.Size = new System.Drawing.Size(262, 69);
+            this.listBoxCompanies.TabIndex = 0;
+            this.listBoxCompanies.SelectedIndexChanged += new System.EventHandler(this.listBoxCompanies_SelectedIndexChanged);
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelDescription.Location = new System.Drawing.Point(380, 1);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(268, 69);
+            this.labelDescription.TabIndex = 1;
+            this.labelDescription.Text = "Changes all vendor default dist GL account references from one GL account to anot" +
+    "her";
+            // 
+            // OpenFileDialogOpen
+            // 
+            this.OpenFileDialogOpen.FileName = "openFileDialog1";
+            // 
+            // buttonReport
+            // 
+            this.buttonReport.BackColor = System.Drawing.Color.LightGray;
+            this.buttonReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReport.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.buttonReport.Location = new System.Drawing.Point(148, 333);
+            this.buttonReport.Name = "buttonReport";
+            this.buttonReport.Size = new System.Drawing.Size(148, 38);
+            this.buttonReport.TabIndex = 16;
+            this.buttonReport.Text = "Exit";
+            this.buttonReport.UseVisualStyleBackColor = false;
+            this.buttonReport.Click += new System.EventHandler(this.buttonReport_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Location = new System.Drawing.Point(457, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 38);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Navy;
+            this.textBox3.Location = new System.Drawing.Point(380, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(268, 22);
+            this.textBox3.TabIndex = 26;
+            this.textBox3.Text = "Select New Discount Account";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Navy;
+            this.textBox2.Location = new System.Drawing.Point(111, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 22);
+            this.textBox2.TabIndex = 25;
+            this.textBox2.Text = "Select Discount Account to Change";
+            // 
+            // cbTo
+            // 
+            this.cbTo.FormattingEnabled = true;
+            this.cbTo.Location = new System.Drawing.Point(380, 193);
+            this.cbTo.Name = "cbTo";
+            this.cbTo.Size = new System.Drawing.Size(268, 21);
+            this.cbTo.TabIndex = 24;
+            this.cbTo.SelectedIndexChanged += new System.EventHandler(this.cbTo_SelectedIndexChanged);
+            // 
+            // cbFrom
+            // 
+            this.cbFrom.FormattingEnabled = true;
+            this.cbFrom.Location = new System.Drawing.Point(109, 193);
+            this.cbFrom.Name = "cbFrom";
+            this.cbFrom.Size = new System.Drawing.Size(252, 21);
+            this.cbFrom.TabIndex = 23;
+            this.cbFrom.SelectedIndexChanged += new System.EventHandler(this.cbFrom_SelectedIndexChanged);
+            // 
+            // statusGroupBox
+            // 
+            this.statusGroupBox.Controls.Add(this.labelCurrentStatus);
+            this.statusGroupBox.Controls.Add(this.progressBar);
+            this.statusGroupBox.Controls.Add(this.labelPercentComplete);
+            this.statusGroupBox.Location = new System.Drawing.Point(111, 76);
+            this.statusGroupBox.Name = "statusGroupBox";
+            this.statusGroupBox.Size = new System.Drawing.Size(537, 73);
+            this.statusGroupBox.TabIndex = 22;
+            this.statusGroupBox.TabStop = false;
+            this.statusGroupBox.Text = "Utility Status:";
+            // 
+            // labelCurrentStatus
+            // 
+            this.labelCurrentStatus.AutoSize = true;
+            this.labelCurrentStatus.Location = new System.Drawing.Point(7, 50);
+            this.labelCurrentStatus.Name = "labelCurrentStatus";
+            this.labelCurrentStatus.Size = new System.Drawing.Size(77, 13);
+            this.labelCurrentStatus.TabIndex = 2;
+            this.labelCurrentStatus.Text = "Current Status:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(7, 27);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(519, 20);
+            this.progressBar.TabIndex = 0;
+            // 
+            // labelPercentComplete
+            // 
+            this.labelPercentComplete.AutoSize = true;
+            this.labelPercentComplete.Location = new System.Drawing.Point(413, 11);
+            this.labelPercentComplete.Name = "labelPercentComplete";
+            this.labelPercentComplete.Size = new System.Drawing.Size(62, 13);
+            this.labelPercentComplete.TabIndex = 0;
+            this.labelPercentComplete.Text = "% Complete";
+            // 
+            // UtilityBaseMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(658, 413);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.cbTo);
+            this.Controls.Add(this.cbFrom);
+            this.Controls.Add(this.statusGroupBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonReport);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.listBoxCompanies);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.LexisNexisLogoPictureBox);
+            this.Controls.Add(this.JurisLogoImageBox);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UtilityBaseMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vendor GL Default Dist Account Updater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.JurisLogoImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexisNexisLogoPictureBox)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.statusGroupBox.ResumeLayout(false);
+            this.statusGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox JurisLogoImageBox;
+        private System.Windows.Forms.PictureBox LexisNexisLogoPictureBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ListBox listBoxCompanies;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialogOpen;
+        private System.Windows.Forms.Button buttonReport;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cbTo;
+        private System.Windows.Forms.ComboBox cbFrom;
+        private System.Windows.Forms.GroupBox statusGroupBox;
+        private System.Windows.Forms.Label labelCurrentStatus;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label labelPercentComplete;
+    }
+}
+
---
+++ JurisUtilityBase/UtilityBaseMain.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Globalization;
+using Gizmox.Controls;
+using JDataEngine;
+using JurisAuthenticator;
+using JurisUtilityBase.Properties;
+using System.Data.OleDb;
+
+namespace JurisUtilityBase
+{
+    public partial class UtilityBaseMain : Form
+    {
+        #region Private  members
+
+        private JurisUtility _jurisUtility;
+
+        #endregion
+
+        #region Public properties
+
+        public string CompanyCode { get; set; }
+
+        public string JurisDbName { get; set; }
+
+        public string JBillsDbName { get; set; }
+
+
+        public string fromGLAccount = "";
+
+        public string toGLAccount = "";
+
+        public string formattingString = "";
+
+        #endregion
+
+        #region Constructor
+
+        public UtilityBaseMain()
+        {
+            InitializeComponent();
+            _jurisUtility = new JurisUtility();
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void LoadCompanies()
+        {
+            var companies = _jurisUtility.Companies.Cast<object>().Cast<Instance>().ToList();
+//            listBoxCompanies.SelectedIndexChanged -= listBoxCompanies_SelectedIndexChanged;
+            listBoxCompanies.ValueMember = "Code";
+            listBoxCompanies.DisplayMember = "Key";
+            listBoxCompanies.DataSource = companies;
+//            listBoxCompanies.SelectedIndexChanged += listBoxCompanies_SelectedIndexChanged;
+            var defaultCompany = companies.FirstOrDefault(c => c.Default == Instance.JurisDefaultCompany.jdcJuris);
+            if (companies.Count > 0)
+            {
+                listBoxCompanies.SelectedItem = defaultCompany ?? companies[0];
+            }
+        }
+
+        #endregion
+
+        #region MainForm events
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void listBoxCompanies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (_jurisUtility.DbOpen)
+            {
+                _jurisUtility.CloseDatabase();
+            }
+            CompanyCode = "Company" + listBoxCompanies.SelectedValue;
+            _jurisUtility.SetInstance(CompanyCode);
+            JurisDbName = _jurisUtility.Company.DatabaseName;
+            JBillsDbName = "JBills" + _jurisUtility.Company.Code;
+            _jurisUtility.OpenDatabase();
+            if (_jurisUtility.DbOpen)
+            {
+                ///GetFieldLengths();
+            }
+
+            string sstest = "select distinct ChtSubAcct1 from ChartOfAccounts order by ChtSubAcct1";
+            DataSet dd = _jurisUtility.RecordsetFromSQL(sstest);
+            if (dd.Tables[0].Rows.Count == 1 && Convert.ToInt32(dd.Tables[0].Rows[0][0].ToString()) == 0)
+                sstest = getSQLBasedOnAccounts(false);
+            else
+                sstest = getSQLBasedOnAccounts(true);
+            string taskCode;
+            cbFrom.ClearItems();
+            string SQLTkpr = sstest;
+            DataSet myRSTkpr = _jurisUtility.RecordsetFromSQL(SQLTkpr);
+
+            if (myRSTkpr.Tables[0].Rows.Count == 0)
+                cbFrom.SelectedIndex = 0;
+            else
+            {
+                foreach (DataTable table in myRSTkpr.Tables)
+                {
+                    cbFrom.Items.Add("****   Update All   ****");
+                    foreach (DataRow dr in table.Rows)
+                    {
+                        taskCode = dr["COA"].ToString();
+                        cbFrom.Items.Add(taskCode);
+                    }
+                }
+
+            }
+
+            string TkprIndex2;
+            cbTo.ClearItems();
+            string SQLTkpr2 = sstest;
+            DataSet myRSTkpr2 = _jurisUtility.RecordsetFromSQL(SQLTkpr2);
+
+
+            if (myRSTkpr2.Tables[0].Rows.Count == 0)
+                cbTo.SelectedIndex = 0;
+            else
+            {
+                foreach (DataTable table in myRSTkpr2.Tables)
+                {
+
+                    foreach (DataRow dr in table.Rows)
+                    {
+                        TkprIndex2 = dr["COA"].ToString();
+                        cbTo.Items.Add(TkprIndex2);
+                    }
+                }
+
+            }
+
+        }
+
+        private string getSQLBasedOnAccounts(bool subAccts)
+        {
+
+            if (!subAccts)
+            {
+                formattingString = "dbo.jfn_FormatMainAcct(ChtMainAcct)";
+                return "select dbo.jfn_FormatMainAcct(ChtMainAcct) + '   ' + ChtDesc as COA from ChartOfAccounts order by dbo.jfn_FormatMainAcct(ChtMainAcct)";
+            }
+            else
+            {
+                formattingString = "dbo.jfn_FormatChartOfAccount(ChtSysNbr)";
+                return "select dbo.jfn_FormatChartOfAccount(ChtSysNbr) + '   ' + ChtDesc as COA from ChartOfAccounts order by dbo.jfn_FormatChartOfAccount(ChtSysNbr)";
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void DoDaFix()
+        {
+            // Enter your SQL code here
+            // To run a T-SQL statement with no results, int RecordsAffected = _jurisUtility.ExecuteNonQueryCommand(0, SQL);
+            // To get an ADODB.Recordset, ADODB.Recordset myRS = _jurisUtility.RecordsetFromSQL(SQL);
+
+
+            string message = "This will change all Vendor Default Dist GL Account references from " + fromGLAccount + "\r\n" + "to " + toGLAccount + ". Are you sure?";
+            if (fromGLAccount.Equals("****"))
+                message = "This will update every Vendor to have Default Dist GL Account " + toGLAccount + ". Are you sure?";
+            DialogResult result = MessageBox.Show(message, "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                UpdateStatus("Updating Vendors...", 1, 1);
+                string SQL = "";
+                if (fromGLAccount.Equals("****"))
+                    SQL = "update vendor set VenDefaultDistAcct = (select ChtSysNbr from ChartOfAccounts where " + formattingString + " = '" + toGLAccount + "')";
+                else
+                    SQL = "update Vendor Set VenDefaultDistAcct = (select ChtSysNbr from ChartOfAccounts where " + formattingString + " = '" + toGLAccount + "') where VenDefaultDistAcct = (select ChtSysNbr from ChartOfAccounts where " + formattingString + " = '" + fromGLAccount + "')";
+                _jurisUtility.ExecuteNonQueryCommand(0, SQL);
+
+
+                MessageBox.Show("The process is complete", "Finished", MessageBoxButtons.OK, MessageBoxIcon.None);
+
+            }
+        }
+        private bool VerifyFirmName()
+        {
+            //    Dim SQL     As String
+            //    Dim rsDB    As ADODB.Recordset
+            //
+            //    SQL = "SELECT CASE WHEN SpTxtValue LIKE '%firm name%' THEN 'Y' ELSE 'N' END AS Firm FROM SysParam WHERE SpName = 'FirmName'"
+            //    Cmd.CommandText = SQL
+            //    Set rsDB = Cmd.Execute
+            //
+            //    If rsDB!Firm = "Y" Then
+            return true;
+            //    Else
+            //        VerifyFirmName = False
+            //    End If
+
+        }
+
+        private bool FieldExistsInRS(DataSet ds, string fieldName)
+        {
+
+            foreach (DataColumn column in ds.Tables[0].Columns)
+            {
+                if (column.ColumnName.Equals(fieldName, StringComparison.OrdinalIgnoreCase))
+                    return true;
+            }
+            return false;
+        }
+
+
+        private static bool IsDate(String date)
+        {
+            try
+            {
+                DateTime dt = DateTime.Parse(date);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool IsNumeric(object Expression)
+        {
+            double retNum;
+
+            bool isNum = Double.TryParse(Convert.ToString(Expression), System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+            return isNum; 
+        }
+
+        private void WriteLog(string comment)
+        {
+            var sql =
+                string.Format("Insert Into UtilityLog(ULTimeStamp,ULWkStaUser,ULComment) Values('{0}','{1}', '{2}')",
+                    DateTime.Now, GetComputerAndUser(), comment);
+            _jurisUtility.ExecuteNonQueryCommand(0, sql);
+        }
+
+        private string GetComputerAndUser()
+        {
+            var computerName = Environment.MachineName;
+            var windowsIdentity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            var userName = (windowsIdentity != null) ? windowsIdentity.Name : "Unknown";
+            return computerName + "/" + userName;
+        }
+
+        /// <summary>
+        /// Update status bar (text to display and step number of total completed)
+        /// </summary>
+        /// <param name="status">status text to display</param>
+        /// <param name="step">steps completed</param>
+        /// <param name="steps">total steps to be done</param>
+
+
+        private void DeleteLog()
+        {
+            string AppDir = Path.GetDirectoryName(Application.ExecutablePath);
+            string filePathName = Path.Combine(AppDir, "VoucherImportLog.txt");
+            if (File.Exists(filePathName + ".ark5"))
+            {
+                File.Delete(filePathName + ".ark5");
+            }
+            if (File.Exists(filePathName + ".ark4"))
+            {
+                File.Copy(filePathName + ".ark4", filePathName + ".ark5");
+                File.Delete(filePathName + ".ark4");
+            }
+            if (File.Exists(filePathName + ".ark3"))
+            {
+                File.Copy(filePathName + ".ark3", filePathName + ".ark4");
+                File.Delete(filePathName + ".ark3");
+            }
+            if (File.Exists(filePathName + ".ark2"))
+            {
+                File.Copy(filePathName + ".ark2", filePathName + ".ark3");
+                File.Delete(filePathName + ".ark2");
+            }
+            if (File.Exists(filePathName + ".ark1"))
+            {
+                File.Copy(filePathName + ".ark1", filePathName + ".ark2");
+                File.Delete(filePathName + ".ark1");
+            }
+            if (File.Exists(filePathName ))
+            {
+                File.Copy(filePathName, filePathName + ".ark1");
+                File.Delete(filePathName);
+            }
+
+        }
+
+            
+
+        private void LogFile(string LogLine)
+        {
+            string AppDir = Path.GetDirectoryName(Application.ExecutablePath);
+            string filePathName = Path.Combine(AppDir, "VoucherImportLog.txt");
+            using (StreamWriter sw = File.AppendText(filePathName))
+            {
+                sw.WriteLine(LogLine);
+            }	
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DoDaFix();
+        }
+
+        private void buttonReport_Click(object sender, EventArgs e)
+        {
+
+            System.Environment.Exit(0);
+          
+        }
+
+        private void cbFrom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fromGLAccount = cbFrom.Text;
+            fromGLAccount = fromGLAccount.Split(' ')[0];
+            if (!String.IsNullOrEmpty(toGLAccount))
+                button1.Enabled = true;
+        }
+
+        private void cbTo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            toGLAccount = cbTo.Text;
+            toGLAccount = toGLAccount.Split(' ')[0];
+            if (!String.IsNullOrEmpty(fromGLAccount))
+                button1.Enabled = true;
+        }
+
+        private void UpdateStatus(string status, long step, long steps)
+        {
+            labelCurrentStatus.Text = status;
+
+            if (steps == 0)
+            {
+                progressBar.Value = 0;
+                labelPercentComplete.Text = string.Empty;
+            }
+            else
+            {
+                double pctLong = Math.Round(((double)step / steps) * 100.0);
+                int percentage = (int)Math.Round(pctLong, 0);
+                if ((percentage < 0) || (percentage > 100))
+                {
+                    progressBar.Value = 0;
+                    labelPercentComplete.Text = string.Empty;
+                }
+                else
+                {
+                    progressBar.Value = percentage;
+                    labelPercentComplete.Text = string.Format("{0} percent complete", percentage);
+                }
+            }
+        }
+    }
+}
